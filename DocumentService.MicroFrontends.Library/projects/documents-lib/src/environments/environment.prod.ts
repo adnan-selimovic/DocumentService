4ec3593aa86c2ba56293aefd81905c757a619115
adnan-selimovic <83953316+adnan-selimovic@users.noreply.github.com>
2022-07-07 10:36:18 +0200

    DocumentService npm package

---
+++ DocumentService.MicroFrontends.Library/projects/documents-lib/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  webAppUrl: 'http://localhost:4200',
+  documentServiceUrl:
+    'https://documentservice-polyglotpersistence.azurewebsites.net',
+  authServiceUrl: 'https://identityserver-documentservice.azurewebsites.net',
+};
---
+++ DocumentService.MicroFrontends.Library/projects/documents-lib/src/public-api.ts
@@ -0,0 +1,16 @@
+/*
+ * Public API Surface of documents-lib
+ */
+
+export * from './lib/documents-lib.service';
+export * from './lib/documents-lib.component';
+export * from './lib/documents-lib.module';
+export * from './lib/documents-lib-routing.module';
+
+export * from './lib/pages/pages.module';
+export * from './lib/pages/pages-routing.module';
+
+export * from './lib/pages/documents/documents.module';
+export * from './lib/pages/documents/documents-routing.module';
+
+export * from './lib/app-core/app-core.module';
